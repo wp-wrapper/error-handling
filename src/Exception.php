a7c74a353ce05b28f217f6c8fa840ae09148722a
Sven Wagener <sven@awesome.ug>
2019-11-14 12:54:51 +0100

    First commit

---
+++ src/Exception.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Exception wrapper.
+ *
+ * @since 1.0.0
+ */
+namespace WPWrapper\ErrorHandling;
+
+/**
+ * Class Exception.
+ *
+ * @since 1.0.0
+ *
+ * @package WPWrapper\ErrorHandling
+ */
+class Exception extends \Exception {
+
+}
---
+++ src/RuntimeException.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Exception wrapper.
+ *
+ * @since 1.0.0
+ */
+namespace WPWrapper\ErrorHandling;
+
+/**
+ * Class RuntimeException.
+ *
+ * @since 1.0.0
+ *
+ * @package WPWrapper\ErrorHandling
+ */
+class RuntimeException extends \RuntimeException {
+
+}
